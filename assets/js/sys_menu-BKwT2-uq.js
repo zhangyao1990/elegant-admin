8b094436e7f28ff6f5d8701d8ca829d6edf14b21
zhangyao <97972619@qq.com>
2024-06-16 21:47:56 +0800

    deploy

--- assets/js/sys_menu-BKwT2-uq.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 MrZhang 提供技术支持
- * Powered by elegant-admin
- * Github https://github.com/zhangyao1990/elegant-admin
- */
-
-import{x as n,P as s}from"./index-C_34-mjI.js";function u(e){return n.post(`${s}/admin/sysMenu/list`,e)}function i(e){return n.post(`${s}/admin/sysMenu/save`,e)}function a(e){return n.put(`${s}/admin/sysMenu/update`,e)}function d(e){return n.delete(`${s}/admin/sysMenu/delete`,e)}export{i as a,d,a as e,u as g};
