3e7b17341fb2e26fc9543c2d9c1186d8e800fb30
zhangyao <97972619@qq.com>
2024-06-16 21:30:23 +0800

    deploy

--- assets/js/header_banner.vue_vue_type_script_setup_true_lang-CKZ_1Pow.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 MrZhang 提供技术支持
- * Powered by elegant-admin
- * Github https://github.com/zhangyao1990/elegant-admin
- */
-
-import{a as n}from"./index-3HRYap5h.js";import{d as r,a as c,c as d,f as e,u as a,a0 as o}from"./@vue-DJ53MWs5.js";const i={class:"flex","items-start":""},l={class:"h-100px w-100px rounded-100px bg-#d4d4d8"},m=["src"],p={class:"mt-2 flex flex-col justify-center md:ml-6 md:mt-0"},_={class:"text-20px"},x={class:"text-emphasis text-16px text-#999 leading-30px"},v=r({name:"HeaderBanner",__name:"header_banner",setup(h){const s=n();return(t,u)=>(c(),d("div",null,[e("div",null,[e("div",i,[e("div",l,[e("img",{class:"h-100px w-100px rounded-100px",src:a(s).avatar,alt:""},null,8,m)]),e("div",p,[e("h1",_,o(t.$t("home.greeting",{userName:a(s).account})),1),e("span",x,o(t.$t("home.weatherDesc")),1)])])])]))}});export{v as _};
