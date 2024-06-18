206ce6c3182781555e4e2842b9aaabb7e741c791
zhangyao <97972619@qq.com>
2024-06-18 18:50:53 +0800

    deploy

--- assets/js/index.vue_vue_type_script_setup_true_lang-BCKt1IV2.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 MrZhang 提供技术支持
- * Powered by elegant-admin
- * Github https://github.com/zhangyao1990/elegant-admin
- */
-
-import{y as s}from"./index-nv2-JPV3.js";import{d as r,a as u,c,R as n}from"./@vue-DJ53MWs5.js";const f=r({name:"Auth",__name:"index",props:{value:{}},setup(t){const o=t;function a(){return s().auth(o.value)}return(e,l)=>(u(),c("span",null,[a()?n(e.$slots,"default",{key:0}):n(e.$slots,"no-auth",{key:1})]))}});export{f as _};
